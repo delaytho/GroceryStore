18cd5027f709553922e8de72b872f00d8fd6d290
Levy12 <38700822+Levy12@users.noreply.github.com>
2018-04-24 16:10:42 -0400

    Update Cart.java

--- Cart.java
+++ Cart.java
@@ -1,17 +1,34 @@
 package model;
 
+import java.text.NumberFormat;
+
 public class Cart {
 
 	private int numProducts;
-
-	public Cart(int numProducts) {
-		this.numProducts = numProducts;
+	//private String currentName;
+	private int currentQuantity;
+	private double currentPrice;
+	
+	Product product;
+	
+	public Cart(){
+		product = null;
 	}
 	
-	public Cart() {
-		this.numProducts = 0;
+	public Cart(Product product){
+		this.product = product;
+		//numProducts = 0;
 	}
 
+	//Product
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 	/**
 	 * @return the numProducts
 	 */
@@ -25,6 +42,39 @@ public class Cart {
 	public void setNumProducts(int numProducts) {
 		this.numProducts = numProducts;
 	}
+	
+//	//Name
+//	public void setProductName(String currentName) {
+//		this.currentName = product.getName();
+//	}
+//
+//	public String getProductName(String currentName)
+//	{
+//		currentName = product.getName();
+//		return currentName;
+//	}
+//	
+	//Quantity
+		public void setCurrentQuantity(int currentQuantity) {
+			this.currentQuantity = product.getQuantity();
+		}
+
+		public int getCurrentQuantity(int currentQuantity)
+		{
+			currentQuantity = product.getQuantity();
+			return currentQuantity;
+		}
+		
+		//Price
+		public void setCurrentPrice(double currentPrice) {
+			this.currentPrice = product.getPrice();
+		}
+
+		public double getCurrentPrice(double currentPrice)
+		{
+			currentPrice = product.getPrice();
+			return currentPrice;
+		}
 
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
@@ -32,5 +82,48 @@ public class Cart {
 	@Override
 	public String toString() {
 		return "Cart [numProducts=" + numProducts + "]";
+	}
+	
+	public String doCart()
+	{
+		String table = "<table border=1>";
+		
+		NumberFormat nf = NumberFormat.getNumberInstance();
+		nf.setMaximumFractionDigits(2);
+		
+		//  top header row
+	    table += "<tr><td>Cart Items</td>\n" + 
+	    		"		<td>Quantity</td>\n" + 
+	    		"		<td>Total</td>\n" + 
+	    		"		<td>Options</td>";
+
+	    // add rows
+	    for (int row = 1; row <= numProducts; row++) {
+	    	
+	    //	String currentName = this.getProductName(currentName);
+	    	//  int currentQuantity = this.getCurrentQuantity(currentQuantity);
+	    //	double currentPrice = this.getCurrentPrice(currentPrice);
+	    	
+	    	table += "<tr>";
+	    	//numProducts
+		    table += "<td>" + row + "</td>";
+		    
+		    //Cart Items
+		    table += "<td> " + product.getName() + "</td>";
+		    
+		    //Quantity
+		    table += "<td> " + product.getQuantity() + "</td>";
+		    
+		    //Total
+		    table += "<td> $" + nf.format(product.calcTotal(currentPrice, currentQuantity)) + "</td>";
+		    
+		    //Options
+		    table += "<td><a href='updateProductForm'</a></td>";
+		    table += "<td><a href='deleteProductForm'</a></td>";
+		    table += "</tr>";
+	    }
+	    
+		table += "</table>";
+		return table;
 	}
 }
